60d3ada8f274b12b2ca0ffbfba02a95359fe90bd
Dan Ormisher <dan.ormisher@open-money.co.uk>
2023-05-04 07:28:25 +0100

    Added extra test coverage for Position class

--- Minefield.Console/Minefield.Tests/PositionTests.cs
+++ Minefield.Console/Minefield.Tests/PositionTests.cs
@@ -5,10 +5,15 @@ namespace Minefield.Tests;
 
 public class PositionTests
 {
-    [Fact]
-    public void ZeroZero_A1()
+    [Theory]
+    [InlineData(0, 0, "A1")]
+    [InlineData(1, 1, "B2")]
+    [InlineData(5, 3, "F4")]
+    [InlineData(10, 1, "K2")]
+    [InlineData(2, 5, "C6")]
+    public void AsChessNotation(int x, int y, string notation)
     {
-        var pos = new Position(0, 0);
-        pos.AsChessNotation().Should().Be("A1");
+        var pos = new Position(x, y);
+        pos.AsChessNotation().Should().Be(notation);
     }
 }
